98970f89a36965a286a64e604daea2bd24270e84
justxyx <635893003@qq.com>
2022-01-12 20:21:44 +0800

    p206

--- p2链表/main.cpp
+++ p2链表/main.cpp
@@ -2,89 +2,30 @@
 using namespace std;
 
 
-class MyLinkedList{
-public:
-    struct LinkNode{
-        int val;
-        LinkNode *next;
-        LinkNode(int val) :val(val), next(nullptr) {};
-    };
-
-    MyLinkedList() {
-        _dummyHead = new LinkNode(0);
-        _size = 0;
-    }
-
-    int get(int index){
-     if ( index > (_size -1) || index < 0){
-         return  -1;
-     }
-     LinkNode *cur = _dummyHead->next;
-        while ( --index){
-            cur = cur->next;
-        }
-        return cur->val;
-    }
-
-    void addAtHead(int val){
-        LinkNode *newNode = new LinkNode(val);
-        newNode->next = _dummyHead->next;
-        _dummyHead->next = newNode;
-        ++ _size;
-    }
-
-    void addAtTail(int val){
-        LinkNode *newNode = new LinkNode(val);
-        LinkNode *curNode = _dummyHead;
-        while ( curNode->next != nullptr){
-            curNode = curNode->next;
-        }
-        curNode->next = newNode;
-        ++ _size;
-    }
-
-    // index 之前加入
-    // 0 就是头结点   size 就是尾结点  大于size 就返回-1
-    void addAtIndex(int index,int val){
-        if (index > _size)
-            return ;
-        LinkNode *newNode = new LinkNode(val);
-        LinkNode *curNode = _dummyHead->next;
-        while ( index --){
-            curNode = curNode->next;
-        }
-        curNode->next = newNode;
-        ++ _size;
-    }
-
-    void deleteAtIndex(int index){
-        if (index >= _size || index < 0)
-            return;
-        LinkNode *curNode = _dummyHead;
-        while ( index --){
-            curNode = curNode->next;
-        }
-        LinkNode *temp = curNode->next;
-        curNode->next = curNode->next->next;
-        delete temp;
-        --_size;
-    }
-
-    void printLinkList(){
-        LinkNode *cur = _dummyHead;
-        while ( cur->next != nullptr){
-            cout << cur->next->val << " ";
-            cur = cur->next;
-        }
-        cout << endl;
-    }
-
-
-private:
-    int _size;
-    LinkNode *_dummyHead;    // 虚指针
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *cur = head;
+        ListNode *pre = nullptr;
+        ListNode *next = nullptr;
+        while (cur){
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
 
 
 int main() {
