9ebb3c4cedf894bf13eacd5ef5e7e6c2d8ee1d1e
justxyx <635893003@qq.com>
2022-01-16 17:45:44 +0800

    p07

--- p2链表/main.cpp
+++ p2链表/main.cpp
@@ -10,6 +10,49 @@ struct ListNode {
 };
 
 
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA = 0;
+        int lenB = 0;
+        ListNode *p = nullptr;
+        p = headA;
+        while ( p != nullptr){
+            p = p->next;
+            ++ lenA;
+        }
+        p = headB;
+        while ( p != nullptr){
+            p = p->next;
+            ++ lenB;
+        }
+
+        int deb = lenA - lenB;
+        if (deb > 0){
+            for (int i = 0; i < deb; ++i) {
+                headA = headA->next;
+            }
+        } else{
+            for (int i = 0; i < deb; ++i) {
+                headB = headB->next;
+            }
+        }
+
+        ListNode *temp = headA;
+        int flag = 1;
+        while (headA != nullptr){
+
+        }
+
+        if (flag){
+            return temp;
+        } else{
+            return nullptr;
+        }
+
+    }
+};
+
 int main() {
 
     return  0;
