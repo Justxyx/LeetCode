a9f1f445a433eea1633f6604f9f3799649cc1899
justxyx <635893003@qq.com>
2022-03-04 23:50:17 +0800

    单调队列

--- p6栈与队列/main.cpp
+++ p6栈与队列/main.cpp
@@ -3,29 +3,86 @@
 using namespace std;
 #include <queue>
 #include <list>
+#include <vector>
 
 
-class Solution {
+/**
+ * 挂了 超出时间限制
+ */
+//class Solution {
+//public:
+//    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//        vector<int>::iterator begin = nums.begin();
+//        vector<int>::iterator end = nums.end();
+//        vector<int>::iterator temp;
+//        vector<int> res;
+//        int max = NULL;
+//
+//        while ((begin+k) <= end){
+//            temp = begin;
+//            for (int i = 0; i < k; ++i) {
+//                if (max == NULL){
+//                    max = *temp;
+//                } else if (max < *temp){
+//                    max = * temp;
+//                }
+//                ++temp;
+//            }
+//            res.push_back(max);
+//            max = NULL;
+//            ++ begin;
+//        }
+//        return res;
+//    }
+//};
+
+
+//单调队列
+class aloneQue{
+private:
+    deque<int> deque;
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        std::sort(nums.begin(), nums.end());
-        list<int> list;
-        vector<int> vector;
-        int count = 1;
-        for (int i = 0; i < nums.size(); ++i) {
-            if ((i+1 < nums.size()) && nums[i] == nums[i+1]){
-                ++count;
-                continue;
-            } else{
-
-            }
+    int pop(){
+        int temp = deque.front();
+        deque.pop_front();
+        return temp;
+    }
+    void push(int x){
+        while (deque.size() > 0 && deque.back() < x){
+            deque.pop_back();
         }
+        deque.push_back(x);
+    }
+    int top(){
+        return deque.front();
     }
 };
 
 
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        aloneQue aloneQue;
+        int i;
+        for ( i = 0; i < k; ++i) {
+            aloneQue.push(nums[i]);
+        }
+//        --i;
+        vector<int> res;
+        int temp;
+        while (i < nums.size()){
+            aloneQue.push(nums[i]);
+            res.push_back(aloneQue.top());
+            if (aloneQue.top() == nums[i+1-k])
+                aloneQue.pop();
+            ++i;
+        }
+        return res;
+    }
+};
 
 int main() {
-
-
+    Solution solution;
+    vector<int> v{1,-1};
+    solution.maxSlidingWindow(v,1);
 }
