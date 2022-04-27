0ba2639bdb58acf526bec470be66cc77f8ff4abc
justxyx <635893003@qq.com>
2022-04-27 21:24:49 +0800

    n皇后

--- p8回溯/main.cpp
+++ p8回溯/main.cpp
@@ -6,138 +6,92 @@ using namespace std;
 #include <set>
 
 
-
-class Solution1 {
+class Solution {
 public:
+    vector<int> path;
+    vector<vector<int>> res;
 
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
-        vector<string> path;
-        vector<vector<string>> res;
-        int len = tickets.size()+1;
-        backtracking(tickets,path,res,len);
+    vector<vector<string>> solveNQueens(int n) {
+        vector<int> bank(n,0);
+        backtracking(n,bank);
 
-//        if (res.size() > 1){
-//            std::sort(res.begin(), res.end(), cmp);
+//        for (const auto &item : res){
+//            for (const auto &i : item){
+//                cout << i << ",";
+//            }
+//            cout << endl;
 //        }
-        if (res.size() == 0)
-            return path;
-        return res[0];
+
+        return trans();
     }
 
-   static bool cmp(vector<string> &s1, vector<string> &s2) { //提供比较函数
-        for (int i = 0; i < s1.size(); ++i) {
-            if (s1[i] == s2[i])
-                continue;
-            else if (s1[i] > s2[i]){
-                return true;
-            } else{
-                return false;
+    vector<vector<string>> trans(){
+        vector<string> line;
+        vector<vector<string>> tostrs;
+        string str = "";
+        for (const auto &item : res){
+            for (const auto &i : item){
+                str = tostr1(i,item.size());
+                line.push_back(str);
             }
+            tostrs.push_back(line);
+            line.clear();
         }
-       return true;
+        return tostrs;
     }
 
-    void backtracking(vector<vector<string>> &tickets, vector<string> &path,vector<vector<string>> &res,int len){
-        if (path.size() == len){
-            if (res.empty()){
-                res.push_back(path);
-            } else{
-                if (cmp(res[0],path)){
-                    res.pop_back();
-                    res.push_back(path);
-                }
-            }
+    string tostr1(int index,int n){
+        string str = "";
+        for (int i = 0; i < n; ++i) {
+            if (i == index)
+                str += "Q";
+            else
+                str += ".";
+        }
+        return str;
+    }
+
+
+    void backtracking(int n,vector<int> &bank){
+        if (path.size() == n)
+            res.push_back(path);
+        if (path.size() > n)
             return;
-        }
 
-        for (int i = 0; i < tickets.size(); ++i) {
-            // path == 0
+        for (int i = 0; i < n; ++i) {
+
             if (path.empty()){
-                if (tickets[i][0] == "JFK"){
-                    path.push_back(tickets[i][0]);
-                    path.push_back(tickets[i][1]);
-                    vector<vector<string>> tic = tickets;
-                    try {
-                        tic.erase(std::find(tic.begin(), tic.end(), tic[i]));
-                    }catch (exception &e){
-                        cout << "---" << endl;
-                    }
-                    backtracking(tic,path,res,len);
-                    path.pop_back();
-                    path.pop_back();
-                } else{
-                    continue;
-                }
+                path.push_back(i);
+                bank[i] = 1;
             } else{
-                // 不为空
-                string city = *(path.end()-1);
-                if (tickets[i][0] == city){
-                    path.push_back(tickets[i][1]);
-                    vector<vector<string>> tic = tickets;
-                    tic.erase(std::find(tic.begin(), tic.end(), tic[i]));
-                    backtracking(tic,path,res,len);
-                    path.pop_back();
-                }
+                if (bank[i] == 1)
+                    continue;
+                int end = path.back();
+                if ( (i == (end+1)) || (i == (end - 1)) )
+                    continue;
+                path.push_back(i);
+                bank[i] = 1;
             }
+
+            backtracking(n,bank);
+            path.pop_back();
+            bank[i] = 0;
         }
         return;
     }
+
+
 };
 
-class Solution2 {
-public:
 
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
-        vector<string> res;
-        int resCity = -1;
-        while (!tickets.empty()){
-            if (res.empty()){
-                for (int i = 0; i < tickets.size(); ++i) {
-                    if (tickets[i][0] == "JFK"){
-                        if (resCity == -1)
-                            resCity = i;
-                        else if (tickets[i][1]<tickets[resCity][1]){
-                            resCity = i;
-                        }
-                    }
-                }
-                res.push_back(tickets[resCity][0]);
-                res.push_back(tickets[resCity][1]);
-                tickets.erase(std::find(tickets.begin(), tickets.end(), tickets[resCity]));
-                resCity = -1;
-                continue;
-            } else{
-                string nextCity = *(res.end()-1);
-                for (int i = 0; i < tickets.size(); ++i) {
-                    if (tickets[i][0] == nextCity){
-                        if (resCity == -1)
-                            resCity = i;
-                        else if (tickets[i][1]<tickets[resCity][1]){
-                            resCity = i;
-                        }
-                    }
-                }
-                res.push_back(tickets[resCity][1]);
-                tickets.erase(std::find(tickets.begin(), tickets.end(), tickets[resCity]));
-                resCity = -1;
-                continue;
-            }
-
-        }
-        return res;
-    }
-};
 
 
 
 int main() {
 
-
-    vector<vector<string>> tickets = {{"EZE","TIA"},{"EZE","AXA"},{"AUA","EZE"},{"EZE","JFK"},{"JFK","ANU"},{"JFK","ANU"},{"AXA","TIA"},{"JFK","AUA"},{"TIA","JFK"},{"ANU","EZE"},{"ANU","EZE"},{"TIA","AUA"}};
-    vector<vector<string>> tics = {{"JFK","STL"},{"JFK","ALO"},{"JFK","EES"}};
-    Solution solution;
-    vector<string>res = solution.findItinerary(tics);
-    for (const auto &item : res)
+Solution solution;
+vector<vector<string>> res =  solution.solveNQueens(4);
+    for (const auto &item : res[0])
         cout << item << endl;
 
 //
