d51920108d196ffdd94ac5b7829da150a3fc2a12
justxyx <635893003@qq.com>
2022-07-05 12:21:05 +0800

    h188 买卖多次股票

--- p10动态规划/main.cpp
+++ p10动态规划/main.cpp
@@ -10,6 +10,95 @@ using namespace std;
 
 
 
+/*
+ *      dp[i][k][0]  第i天， 交易了k次， 手中没有股票
+ *      dp[i][k][1]         交易了k次， 手中有股票
+ *
+ */
+
+// 之买卖一次股票
+class Solution1 {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(2,0)));
+        dp[0][0][0] = 0;  // 第0天， 已经交易了0次 手中没有股票 那就等于0
+        dp[0][1][1] = -prices[0];  // 第0天 已经交易了1 次 手中有股票
+        for (int i = 0; i < prices.size(); ++i) {
+            dp[i][0][0] = 0;
+        }
+
+
+        for (int i = 1; i < prices.size(); ++i) {
+            // 第i天不持有
+            dp[i][1][0] = max(dp[i-1][1][0], dp[i-1][1][1] + prices[i]);
+
+            // 第i天持有
+            dp[i][1][1] = max(dp[i-1][1][1], dp[i-1][0][0] - prices[i]);
+        }
+
+        return dp[prices.size()-1][1][0];
+    }
+};
+
+// 可买卖两次股票
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(3, vector<int>(2,0)));
+        for (int i = 0; i < prices.size(); ++i) {
+            dp[i][0][0] = 0;   // 第i天， 发生了0次交易 手中没有股票  值为0
+        }
+
+        dp[0][1][1] = -prices[0];
+        dp[0][1][0] = 0;
+        dp[0][2][1] = -prices[0];
+        dp[0][2][0] = 0;
+
+        for (int i = 1; i < prices.size(); ++i) {
+            // 发生了一次交易
+            // 不持有
+            dp[i][1][0] = max(dp[i-1][1][0], dp[i-1][1][1] + prices[i]);
+            // 持有
+            dp[i][1][1] = max(dp[i-1][1][1], dp[i-1][0][0] - prices[i]);
+
+            // 发生了两次交易
+            // 不持有
+            dp[i][2][0] = max(dp[i-1][2][0], dp[i-1][2][1] + prices[i]);
+            // 持有
+            dp[i][2][1] = max(dp[i-1][2][1], dp[i-1][1][0] - prices[i]);
+        }
+        return max(dp[prices.size()-1][1][0], dp[prices.size()-1][2][0]);
+    }
+};
+
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if (prices.size() < 2)
+            return 0;
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(k+1, vector<int>(2,0)));
+        for (int i = 0; i < prices.size(); ++i) {
+            dp[i][0][0] = 0;
+        }
+        for (int i = 1; i <= k; ++i) {
+            dp[0][i][1] = -prices[0];
+            dp[0][i][0] = 0;
+        }
+
+        int maxx = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            for (int j = 1; j <= k; ++j) {
+                // 不持有
+                dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+                // 持有
+                dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i]);
+                maxx = max(dp[i][j][0], dp[i][j][1]);
+            }
+        }
+        return maxx;
+    }
+};
 
 
 int main() {
@@ -18,9 +107,10 @@ int main() {
 //    string s2 = "jay";
 //    s1.swap(s2);
 //    cout << s1 << s2 << endl;
-
     Solution solution;
-    cout << solution.longestPalindromeSubseq("cbbd");
+    vector<int> v{2,4,2};
+    cout << solution.maxProfit(2, v);
+//    cout << solution.maxProfit(v);
     return 0;
 }
 
