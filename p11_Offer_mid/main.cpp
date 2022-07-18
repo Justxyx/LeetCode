a7da75c8ffbc91c5ecc330dc77119a78cf372c06
justxyx <635893003@qq.com>
2022-07-18 22:16:52 +0800

    day 08

--- p11_Offer_mid/main.cpp
+++ p11_Offer_mid/main.cpp
@@ -13,13 +13,56 @@ using namespace std;
          TreeNode(int x) : val(x), left(NULL), right(NULL) {}
      };
 
+class Solution1 {
+public:
+    int fib(int n) {
+        if (n <= 1)
+            return n;
+        vector<int> fn(n+1,0);
+        fn[0] = 0;
+        fn[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            fn[i] = (fn[i-1] + fn[i-2]) % 1000000007;
+        }
+        return fn.back();
+    }
+};
+
+class Solution2 {
+public:
+    int numWays(int n) {
+        if (n < 2)
+            return 1;
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <=n ; ++i) {
+            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+        }
+        return dp[n];
+    }
+};
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty())
+            return 0;
+        int has = -prices[0];
+        int no_has = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            has = max(has, -prices[i]);
+            no_has = max(no_has, prices[i] + has);
+        }
+        return no_has;
+    }
+};
+
+
 
 int main() {
-    TreeNode node(1);
-    node.left = new TreeNode(2);
-    node.right = new TreeNode(2);
-
-    TreeNode res(3);
+    vector<int> v{7,1,5,3,6,4};
     Solution solution;
-    cout << solution.isSymmetric(&node);
+    cout << solution.maxProfit(v);
 }
