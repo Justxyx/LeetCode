e4aeac68603d845f9d75d52c4517215ffc792942
justxyx <635893003@qq.com>
2022-07-21 14:35:16 +0800

    二刷 回溯

--- p12 二刷/main.cpp
+++ p12 二刷/main.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 using namespace std;
 #include <numeric>
-#include "vector.h"
-
+#include <set>
+#include <list>
 // 1. 组合问题  其核心思想 for 循环横向遍历 递归纵向遍历
 class Solution77 {
 public:
@@ -152,6 +152,192 @@ public:
 };
 
 
+// 2. 分割问题
+// 这道题想了好久
+class Solution131 {
+public:
+    vector<vector<string>> res;
+    vector<string> path;
+    vector<vector<string>> partition(string s) {
+        backtracking(s, 0);
+        return res;
+    }
+    void backtracking(string s, int index) {
+        if (index == s.size())
+        {
+            res.push_back(path);
+            return;
+        }
+        if (index >= s.size())
+            return;
+        for (int i = index; i < s.size(); ++i) {
+            string ss = string(s.begin()+index, s.begin()+i+1);
+            if (isPart(ss)) {
+                cout << ss << endl;
+                path.push_back(ss);
+                backtracking(s, i+1);
+                path.pop_back();
+            }
+        }
+    }
+    bool isPart(string str) {
+        int begin = 0;
+        int end = str.size() - 1;
+        bool b = true;
+        while (begin <= end) {
+            if (str[begin] != str[end])
+                b = false;
+            ++ begin;
+            -- end;
+        }
+        return b;
+    }
+};
+// 跟上一题差不多 都是分割问题
+class Solution93 {
+public:
+    vector<string> path;
+    vector<string> res;
+    vector<string> restoreIpAddresses(string s) {
+        backtracking(s, 0);
+        return res;
+    }
+    void backtracking(string s, int index) {
+        if (index == s.size() && path.size() == 4)
+        {
+            string path_res = "";
+            for (const auto &item : path) {
+                path_res += item;
+                path_res += ".";
+            }
+            path_res.resize(path_res.size()-1);
+            res.push_back(path_res);
+        }
+        for (int i = index; i < s.size(); ++i) {
+
+            if (path.size() >= 4)
+                return;
+            string ss(s.begin()+index, s.begin()+i+1);
+            int ss_int = atoi(ss.c_str());
+            if ((ss.size() == 1) || (ss[0] != '0' && ss_int < 256 && ss.size() < 4)) {
+                cout << ss_int << endl;
+                path.push_back(ss);
+                backtracking(s, i+1);
+                path.pop_back();
+            }
+        }
+        return;
+    }
+};
+
+
+// 3.子集问题
+class Solution78 {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtracking(nums, 0);
+        return res;
+    }
+    void backtracking(vector<int> &nums, int index) {
+        if (index >= nums.size())
+            return;
+        res.push_back(path);
+        for (int i = index; i < nums.size(); ++i) {
+            path.push_back(nums[i]);
+            backtracking(nums, i+1);
+            path.pop_back();
+        }
+        return;
+    }
+
+};
+// 子集去重复
+class Solution90 {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        backtracking(nums, 0);
+        return res;
+    }
+    void backtracking(vector<int> &nums, int index) {
+        res.push_back(path);
+        if (index >= nums.size())
+            return;
+        for (int i = index; i < nums.size(); ++i) {
+            if (i != index && nums[i-1] == nums[i])
+                continue;
+            path.push_back(nums[i]);
+            backtracking(nums, i+1);
+            path.pop_back();
+        }
+        return;
+    }
+};
+/*
+ * 这题目有空再看看  非递增的序列 子集去重问题 用set
+ */
+class Solution491 {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        backtracking(nums, 0);
+        return res;
+    }
+    void backtracking(vector<int> &nums, int index) {
+        set<int> sets;
+        if (path.size() >= 2)
+            res.push_back(path);
+        for (int i = index; i < nums.size(); ++i) {
+            if (true) {
+                if (sets.find(nums[i]) == sets.end()) {
+                    sets.insert(nums[i]);
+                } else {
+                    continue;
+                }
+            }
+            if (path.empty() || nums[i] >= path.back()) {
+                path.push_back(nums[i]);
+                backtracking(nums, i+1);
+                path.pop_back();
+            }
+        }
+    }
+};
+
+
+//4. 排列问题
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+    int size = 0;
+    vector<vector<int>> permute(vector<int>& nums) {
+        size = nums.size();
+        backtracking(nums, -1);
+        return res;
+    }
+    void backtracking(vector<int> nums, int value) {
+        if (path.size() == size)
+            res.push_back(path);
+        if (path.size() > size)
+            return;
+        if (value >= 0)
+            nums.erase(nums.begin() + value);
+        for (int i = 0; i < nums.size(); ++i) {
+            path.push_back(nums[i]);
+            backtracking(nums, i);
+            path.pop_back();
+        }
+    }
+};
+
 int main() {
+    vector<int> v{4,6,7,7};
+    Solution solution;
     return 0;
 }
