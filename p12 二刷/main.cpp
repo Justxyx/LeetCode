a7d817704507a5433f4a22b27ff8709b91836280
justxyx <635893003@qq.com>
2022-08-24 20:50:14 +0800

    dp

--- p12 二刷/main.cpp
+++ p12 二刷/main.cpp
@@ -711,12 +711,12 @@ public:
     }
 };
 
-class Solution {
+class Solution115 {
 public:
     int numDistinct(string s, string t) {
         if (s.empty() || t.empty())
             return 0;
-        s.swap(t);
+//        s.swap(t);
         vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, 0));
         // 初始化
         for (int i = 1; i <= t.size(); ++i) {
@@ -728,7 +728,7 @@ public:
         for (int i = 2; i < dp.size(); ++i) {
             for (int j = 1; j < dp[0].size(); ++j) {
                 if (s[i-1] == t[j-1])
-                    dp[i][j] = max( dp[i][j-1] + 1, dp[i-1][j-1]);
+                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
                 else
                     dp[i][j] = dp[i][j-1];
             }
@@ -736,9 +736,73 @@ public:
         return dp.back().back();
     }
 };
+
+
+class Solution583 {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, 0));
+        // 初始化
+        for (int i = 0; i < dp[0].size(); ++i) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i < dp.size(); ++i) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i < dp.size(); ++i) {
+            for (int j = 1; j < dp[0].size(); ++j) {
+                if (word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min(min(dp[i][j-1]+1, dp[i-1][j]+1), dp[i-1][j-1] + 2);
+            }
+        }
+        return dp.back().back();
+    }
+};
+
+class Solution {
+public:
+    int count = 0;
+    vector<char> path;
+    int countSubstrings(string s) {
+        backtracking(s, 0);
+        return count;
+    }
+
+    void backtracking(string &str, int index) {
+        if (judeg()) {
+            ++ count;
+            string temp = string(path.begin(), path.end());
+            cout << temp << endl;
+        }
+        if (index >= str.size())
+            return;
+        for (int i = index; i < str.size(); ++i) {
+            path.push_back(str[i]);
+            backtracking(str, i+1);
+            path.clear();
+        }
+    }
+
+    bool judeg() {
+        if (path.empty())
+            return false;
+        if (path.size() == 1)
+            return true;
+        int left = 0;
+        int right = path.size()-1;
+        while (left < right) {
+            if (path[left] != path[right])
+                return false;
+            ++ left;
+            -- right;
+        }
+        return true;
+    }
+};
+
 int main() {
     Solution solution;
-    solution.numDistinct("rabbbit", "rabbit");
-
-
+    cout << solution.countSubstrings("aaa");
 }
